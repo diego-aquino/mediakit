5da8eecebe6c954836703713a52d8e6f6598b0ce
Diego Aquino <diego.cruz001@gmail.com>
2020-11-15 20:46:40 -0300

    Add safe len() for strings with ANSI escape codes

--- mediakit/utils/format.py
+++ mediakit/utils/format.py
@@ -1,3 +1,5 @@
+import re
+
 def limit_text_length(text, limit_of_caracters):
     if len(text) > limit_of_caracters:
         partial_text_with_ellipsis = f'{text[:limit_of_caracters - 3]}...'
@@ -5,3 +7,14 @@ def limit_text_length(text, limit_of_caracters):
         return partial_text_with_ellipsis
 
     return text
+
+def len_ansi_safe(string):
+    ansi_escape_codes_regex = r'\x1b\[[;\d]*[A-Za-z]'
+
+    string_striped_of_ansi_codes = re.sub(
+        ansi_escape_codes_regex,
+        '',
+        string
+    )
+
+    return len(string_striped_of_ansi_codes)
