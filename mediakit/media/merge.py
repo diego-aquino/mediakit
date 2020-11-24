acc4e7537922f2dfee518e596887eac162fc8eb9
Diego Aquino <diego.cruz001@gmail.com>
2020-11-24 10:52:21 -0300

    Get the output format as a parameter on `merge`

--- mediakit/media/merge.py
+++ mediakit/media/merge.py
@@ -5,10 +5,13 @@ from mediakit.utils.commands import run_command_in_background
 from mediakit.constants import FFMPEG_BINARY
 
 
-def merge_video_and_audio(video_path, audio_path, output_file_path):
+def merge_video_and_audio(
+    video_path,
+    audio_path,
+    output_file_path,
+    output_format='mp4'
+):
     final_output_file_path = increment_filename_if_exists(output_file_path)
-
-    output_format = path.basename(video_path).split('.')[-1]
 
     command = (
         f'{FFMPEG_BINARY} -i {video_path} -i {audio_path} '
