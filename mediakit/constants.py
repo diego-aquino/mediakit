c9a768c7008bd4c43a42a28ff17ed64d7e8ad4a6
Diego Aquino <diego.cruz001@gmail.com>
2020-12-01 18:28:17 -0300

    Add download formats and update definitions

--- mediakit/constants.py
+++ mediakit/constants.py
@@ -3,7 +3,10 @@ from .utils.ffmpeg import get_ffmpeg_binary
 
 FFMPEG_BINARY = get_ffmpeg_binary()
 
+DOWNLOAD_FORMATS = { 'videoaudio', 'audio', 'videoonly' }
+
 VIDEO_DEFINITIONS = {
+    'max': { 'next': '2160p' },
     '2160p': { 'next': '1440p' },
     '1440p': { 'next': '1080p' },
     '1080p': { 'next': '720p' },
@@ -19,8 +22,18 @@ VIDEO_DEFINITIONS_ALIASES = {
 }
 
 AUDIO_DEFINITIONS = {
+    'max': { 'next': '160kbps' },
     '160kbps': { 'next': '128kbps' },
     '128kbps': { 'next': '70kbps' },
     '70kbps': { 'next': '50kbps' },
     '50kbps': { 'next': None }
 }
+
+AVAILABLE_DEFINITIONS = set()
+for definition_group in [
+    VIDEO_DEFINITIONS,
+    VIDEO_DEFINITIONS_ALIASES,
+    AUDIO_DEFINITIONS
+]:
+    for definition in definition_group:
+        AVAILABLE_DEFINITIONS.add(definition)
