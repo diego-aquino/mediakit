560fd7856c8cf3d4c4528a9d618511483f246923
Diego Aquino <diego.cruz001@gmail.com>
2020-12-01 16:18:45 -0300

    Wrap paths in quotes to prevent execution problems

--- mediakit/media/convert.py
+++ mediakit/media/convert.py
@@ -21,7 +21,7 @@ def merge_video_and_audio(
     final_output_file_path = increment_filename_if_exists(output_file_path)
 
     command = (
-        f'{FFMPEG_BINARY} -i {video_path} -i {audio_path} '
+        f'{FFMPEG_BINARY} -i "{video_path}" -i "{audio_path}" '
         f'-vcodec copy -f {output_format} '
         f'"{final_output_file_path}"'
     )
@@ -33,7 +33,7 @@ def convert_media(file_path, output_file_path, output_format, options=[]):
     final_output_file_path = increment_filename_if_exists(output_file_path)
 
     command = (
-        f'{FFMPEG_BINARY} -i {file_path} '
+        f'{FFMPEG_BINARY} -i "{file_path}" '
         + ('-vcodec copy ' if output_format in VIDEO_FORMATS else '')
         + f'-f {output_format} '
         + f'"{final_output_file_path}" '
