e32efc73430b0bd0f7fe91b467ca9cd35e7555c8
Diego Aquino <diego.cruz001@gmail.com>
2021-02-22 12:08:51 -0300

    Update project version to v0.5.1

--- mediakit/info.py
+++ mediakit/info.py
@@ -1,10 +1,10 @@
 name = 'Mediakit'
-version = '0.5.0'
+version = '0.5.1'
 description = 'Download YouTube videos fast, directly from the command line'
 author = 'Diego Aquino'
 author_email = 'diegocruzdeaquino@gmail.com'
 url = 'https://github.com/diego-aquino/mediakit'
-download_url = 'https://github.com/diego-aquino/mediakit/archive/v0.5.0.tar.gz'
+download_url = 'https://github.com/diego-aquino/mediakit/archive/v0.5.1.tar.gz'
 license = 'MIT'
 copyright = 'Copyright 2020 Diego Aquino'
 
