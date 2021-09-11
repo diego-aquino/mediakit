d64d1d80e8a57fbccd889c99d308911a692ec459
Diego Aquino <diegocruzdeaquino@gmail.com>
2021-09-11 19:40:48 -0300

    Migrate project license to GPL-3.0

--- mediakit/info.py
+++ mediakit/info.py
@@ -1,11 +1,11 @@
-name = 'Mediakit'
-version = '0.5.4'
-description = 'Download YouTube videos fast, directly from the command line'
-author = 'Diego Aquino'
-author_email = 'diegocruzdeaquino@gmail.com'
-url = 'https://github.com/diego-aquino/mediakit'
-download_url = 'https://github.com/diego-aquino/mediakit/archive/v0.5.4.tar.gz'
-license = 'MIT'
-copyright = 'Copyright 2021 Diego Aquino'
+name = "Mediakit"
+version = "0.5.4"
+description = "Download YouTube videos fast, directly from the command line"
+author = "Diego Aquino"
+author_email = "diegocruzdeaquino@gmail.com"
+url = "https://github.com/diego-aquino/mediakit"
+download_url = "https://github.com/diego-aquino/mediakit/archive/v0.5.4.tar.gz"
+license = "GPL-3.0"
+copyright = "Copyright 2021 Diego Aquino"
 
-temp_filename = '.mediakit_download'
+temp_filename = ".mediakit_download"
--- setup.py
+++ setup.py
@@ -5,9 +5,9 @@ from setuptools import setup, find_packages
 from mediakit import info
 
 current_dirname = path.dirname(path.abspath(__file__))
-readme_path = path.join(current_dirname, 'README.md')
+readme_path = path.join(current_dirname, "README.md")
 
-with open(readme_path, encoding='utf-8') as readme:
+with open(readme_path, encoding="utf-8") as readme:
     long_description = readme.read()
 
 setup(
@@ -15,7 +15,7 @@ setup(
     version=info.version,
     description=info.description,
     long_description=long_description,
-    long_description_content_type='text/markdown',
+    long_description_content_type="text/markdown",
     author=info.author,
     author_email=info.author_email,
     url=info.url,
@@ -24,46 +24,39 @@ setup(
     packages=find_packages(),
     include_package_data=True,
     keywords=[
-        'youtube',
-        'media',
-        'video',
-        'audio',
-        'download',
-        'convert',
-        'command line',
-        'cli'
+        "youtube",
+        "media",
+        "video",
+        "audio",
+        "download",
+        "convert",
+        "command line",
+        "cli",
     ],
-    python_requires='>=3.6',
+    python_requires=">=3.6",
     install_requires=[
-        'pytube>=11.0.0',
-        'clint',
-        'imageio',
-        'imageio-ffmpeg',
-        'colorama',
-        'wheel'
+        "pytube>=11.0.0",
+        "clint",
+        "imageio",
+        "imageio-ffmpeg",
+        "colorama",
+        "wheel",
     ],
-    entry_points={
-        'console_scripts': [
-            'mediakit=mediakit:main'
-        ]
-    },
+    entry_points={"console_scripts": ["mediakit=mediakit:main"]},
     classifiers=[
-        'Development Status :: 4 - Beta',
-
-        'Environment :: Console',
-        'Intended Audience :: End Users/Desktop',
-        'License :: OSI Approved :: MIT License',
-        'Natural Language :: English',
-        'Operating System :: Unix',
-
-        'Programming Language :: Python :: 3.6',
-        'Programming Language :: Python :: 3.7',
-        'Programming Language :: Python :: 3.8',
-        'Programming Language :: Python :: 3.9',
-
-        'Topic :: Multimedia :: Video',
-        'Topic :: Multimedia :: Video :: Conversion',
-        'Topic :: Multimedia :: Sound/Audio',
-        'Topic :: Multimedia :: Sound/Audio :: Conversion'
-    ]
+        "Development Status :: 4 - Beta",
+        "Environment :: Console",
+        "Intended Audience :: End Users/Desktop",
+        "License :: OSI Approved :: GNU General Public License v3 (GPLv3)",
+        "Natural Language :: English",
+        "Operating System :: Unix",
+        "Programming Language :: Python :: 3.6",
+        "Programming Language :: Python :: 3.7",
+        "Programming Language :: Python :: 3.8",
+        "Programming Language :: Python :: 3.9",
+        "Topic :: Multimedia :: Video",
+        "Topic :: Multimedia :: Video :: Conversion",
+        "Topic :: Multimedia :: Sound/Audio",
+        "Topic :: Multimedia :: Sound/Audio :: Conversion",
+    ],
 )
